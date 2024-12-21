5807bd0ac22a5e0654a258332b78738eaf02b9bc
crisdats <147233981+crisdats@users.noreply.github.com>
2024-12-21 15:46:05 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'RIFF API'// yourname
+global.MONGO_DB_URI = "mongodb://mongo:sZPYphOSmaoGCSMIrVmRFQEauYIMCnLW@junction.proxy.rlwy.net:18842" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "666" //isi apa aja bebas
+global.your_email = "adasdsadsdsadsdsadasdsadsddsa@gmail.com" //email
+global.email_password = "@Userzeroyt7" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
